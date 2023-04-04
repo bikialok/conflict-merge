bb1b1d137e2abf9394748fe0cdbd03c727d8f283
bikialok <128984222+bikialok@users.noreply.github.com>
2023-04-04 09:43:31 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,18 @@
+public class   StudentDetails{
+           public static void main(String[] args){
+                //declaration of data type
+                int studentRollNumber = 20110;
+                String studentName = "Alok Kumar Nayak";
+                String studentCourse = "Btech Electrical Eng.";
+                double courseFee = 80078.00;
+                String collegeName = "Nalanda Engineering College";
+                String collegeLocation = "Bhubaneswar,odisha";
+                System.out.println("Welcome To Student Details");
+                System.out.println("Student-Roll-Number::"+studentRollNumber);
+                System.out.println("Student-Name::"+studentName);
+                System.out.println("Student-Course::"+studentCourse);
+                System.out.println("Course-Fee::"+courseFee);
+                System.out.println("College-Name::"+collegeName);
+                System.out.println("College-Location::"+collegeLocation);  
+    }
+}

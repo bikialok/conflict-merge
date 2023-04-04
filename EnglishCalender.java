5e52d225a9571af438f3752d8549ffb6e76c7d72
bikialok <128984222+bikialok@users.noreply.github.com>
2023-04-04 16:44:59 +0530

    Add files via upload

---
+++ EnglishCalender.java
@@ -0,0 +1,18 @@
+class   EnglishCalender{
+    public static void main(String[] args){
+         System.out.println("====TWELVE MONTH OF ENGLISH CALENDER====");
+         System.out.println("=> JANUARY");
+         System.out.println("=> FEBRUARY");
+         System.out.println("=> MARCH");
+         System.out.println("=> APRIL");
+         System.out.println("=> MAY");
+         System.out.println("=> JUNE");
+         System.out.println("=> JULY");
+         System.out.println("=> AUGUST");
+         System.out.println("=> SEPTEMBER");
+         System.out.println("=> OCTOBER");
+         System.out.println("=> NOVEMBER");
+         System.out.println("=> DECEMBER"); 
+    }
+
+}
